17670dc6d2baa231e180af1e568d77453c234600
mognedy <mognedy@gmail.com>
2020-06-25 14:05:20 +0200

    install @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,16 +6,18 @@ import { User } from './users/user.entity';
 
 import { Connection } from 'typeorm';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'password',
-      database: 'test',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASS,
+      database: process.env.MYSQL_DB,
       entities: [User],
       synchronize: true,
     }),
